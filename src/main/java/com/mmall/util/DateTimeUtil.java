2afbd7e259aa77f8b0f04a67bdd461f5444eb0bf
imooc <1713157566@qq.com>
2019-05-16 16:59:18 +0800

    mmall

---
+++ src/main/java/com/mmall/util/DateTimeUtil.java
@@ -0,0 +1,45 @@
+package com.mmall.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
+
+public class DateTimeUtil {
+
+    public static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+
+    //date -> str
+    public static String dateToStr(Date date,String formatStr){
+        if(date == null){
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(formatStr);
+    }
+
+    //str -> date
+    public static Date strToDate(String dateTimeStr,String formatStr){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(formatStr);
+        DateTime dateTime =dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+    }
+
+    public static String dateToStr(Date date){
+        if(date == null){
+            return StringUtils.EMPTY;
+        }
+        DateTime dateTime = new DateTime(date);
+        return dateTime.toString(STANDARD_FORMAT);
+    }
+
+    public static Date strToDate(String dateTimeStr){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(STANDARD_FORMAT);
+        DateTime dateTime =dateTimeFormatter.parseDateTime(dateTimeStr);
+        return dateTime.toDate();
+    }
+
+}
